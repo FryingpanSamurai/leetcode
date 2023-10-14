6b384ceb04486d633e4460e236a18c402e2d07ec
bpatricka <bpatricka@live.com>
2023-10-14 11:46:42 -0600

    lemonade

---
+++ lemonadeChangeALT.py
@@ -0,0 +1,23 @@
+class Solution:
+  def lemonadeChange(self, bills: list[int]) -> bool: 
+    tens = fives = 0
+    for bill in bills:
+      if bill == 5:
+        fives += 1
+      elif bill == 10 and fives >= 1:
+        tens += 1
+        fives -= 1
+      elif bill == 20 and fives >= 3:
+        fives -= 3
+      elif bill == 20 and (fives >= 1 and tens >= 1):
+        fives -= 1
+        tens -= 1
+      else:
+        return False
+
+
+    return True
+if __name__ == '__main__':
+  test = Solution()
+  bills = [5,5,5,10,20]
+  test.lemonadeChange(bills)
